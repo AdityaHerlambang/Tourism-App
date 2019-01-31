217aa816a83809e6b31ad29177b5edcc28a6171f
Aditya Herlambang <herlambangadt@gmail.com>
2019-01-31 14:04:43 +0800

    Login and Register

--- app/src/main/java/com/artace/tourism/TourDetailActivity.java
+++ app/src/main/java/com/artace/tourism/TourDetailActivity.java
@@ -1,5 +1,6 @@
 package com.artace.tourism;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
@@ -11,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
